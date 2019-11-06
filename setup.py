e11e8d40252da796de64200ec073d2394c464a62
Giovanni Saccarola <giovanni.saccarola@hotmail.it>
2019-11-06 17:21:50 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'gufo',
+    version = '0.1dev',
+    packages = ['window'],
+    license = 'GNU license',
+    long_description = open('README.md').read(),
+)
